14ec2a57b24c42df084eec6fa7fd6797f6684799
Anwer Khan <deepwatrcreatur@gmail.com>
2026-01-10 23:33:30 -0500

    fix: correct fnox source and cargo hashes

--- flake.nix
+++ flake.nix
@@ -6,8 +6,14 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         version = "1.7.0";
@@ -21,22 +27,25 @@
             owner = "jdx";
             repo = "fnox";
             rev = "v${version}";
-            hash = "sha256-0g0bqx2qb17g1q8xvml7kp3z6yn2laivqqhvs5y5mhavl7saq4af";
+            hash = "sha256-ThGs9KFbwVp80RtivKOiwnrzx52H1t0RDu+EhUXHCzw=";
           };
 
           # Update this hash after the first failed build
-          cargoHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+          cargoHash = "sha256-U3poZWMd1AMYv1v/rCoCuL24mxQOo++1WkLD/SxwNvU=";
 
           nativeBuildInputs = with pkgs; [
             pkg-config
           ];
 
-          buildInputs = with pkgs; [
-            openssl
-          ] ++ lib.optionals stdenv.isDarwin [
-            darwin.apple_sdk.frameworks.Security
-            darwin.apple_sdk.frameworks.SystemConfiguration
-          ];
+          buildInputs =
+            with pkgs;
+            [
+              openssl
+            ]
+            ++ lib.optionals stdenv.isDarwin [
+              darwin.apple_sdk.frameworks.Security
+              darwin.apple_sdk.frameworks.SystemConfiguration
+            ];
 
           doCheck = false; # Skip tests for faster builds
 
